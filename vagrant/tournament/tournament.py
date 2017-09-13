acbcca4c1b8f17c31a667bdfb28f0a8b476bbe86
Arslan ALAN <alan.arslan@gmail.com>
2017-09-13 12:06:12 +0300

    Update tournament.py and tournament.sql related to tournament project     - tournament.sql: sql commands for initializing database (creating db, tables, and views)     - tournament.py: functions for connecting, inserting, and selecting operations on db, only the pairing of players is iplemented with python code.

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
-# 
+#
 # tournament.py -- implementation of a Swiss-system tournament
 #
 
 import psycopg2
+import bleach
 
 
 def connect():
@@ -13,25 +14,47 @@ def connect():
 
 def deleteMatches():
     """Remove all the match records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM matches;")
+    conn.commit()
+    conn.close()
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("DELETE FROM players;")
+    conn.commit()
+    conn.close()
 
 
 def countPlayers():
     """Returns the number of players currently registered."""
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT COUNT ( * ) FROM players;")
+    totalPlayers = c.fetchall()
+    conn.close()
+    return int(totalPlayers[0][0])
 
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
-  
+
     The database assigns a unique serial id number for the player.  (This
     should be handled by your SQL database schema, not in your Python code.)
-  
+
     Args:
       name: the player's full name (need not be unique).
     """
+    conn = connect()
+    c = conn.cursor()
+    name = bleach.clean(name)
+    c.execute("INSERT INTO players VALUES ( %s )", (name,))
+    conn.commit()
+    conn.close()
 
 
 def playerStandings():
@@ -47,6 +70,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT * FROM standings")
+    standings = c.fetchall()
+    conn.close()
+    return standings
 
 
 def reportMatch(winner, loser):
@@ -56,16 +85,21 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
- 
- 
+    conn = connect()
+    c = conn.cursor()
+    c.execute("INSERT INTO matches (winner_id, looser_id) VALUES ( %s, %s )", (winner, loser, ))
+    conn.commit()
+    conn.close()
+
+
 def swissPairings():
     """Returns a list of pairs of players for the next round of a match.
-  
+
     Assuming that there are an even number of players registered, each player
     appears exactly once in the pairings.  Each player is paired with another
     player with an equal or nearly-equal win record, that is, a player adjacent
     to him or her in the standings.
-  
+
     Returns:
       A list of tuples, each of which contains (id1, name1, id2, name2)
         id1: the first player's unique id
@@ -73,5 +107,16 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    conn = connect()
+    c = conn.cursor()
+    c.execute("SELECT * FROM standings")
+    rows = c.fetchall()
+    conn.close()
+    rowIndex = 0
+    pairings = []
+    while rowIndex < len(rows):
+        pairings.append((rows[rowIndex][0], rows[rowIndex][1], rows[rowIndex+1][0], rows[rowIndex+1][1]))
+        rowIndex = rowIndex + 2
+    return pairings
 
 
--- vagrant/tournament/tournament.sql
+++ vagrant/tournament/tournament.sql
@@ -5,5 +5,28 @@
 --
 -- You can write comments in this file by starting them with two dashes, like
 -- these lines here.
+--CREATE TABLE posts ( content TEXT,
+--                     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+--                     id SERIAL );
+DROP DATABASE IF EXISTS tournament;
+CREATE DATABASE tournament;
+\c tournament;
+CREATE TABLE players (  name TEXT,
+                        id SERIAL PRIMARY KEY );
 
+CREATE TABLE matches (  winner_id INTEGER REFERENCES players ( id ),
+                        looser_id INTEGER REFERENCES players ( id ),
+                        time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        PRIMARY KEY (winner_id, looser_id, time) );
 
+CREATE VIEW standings AS
+    SELECT players.id AS id, players.name AS name,
+        (SELECT count(*) AS wins FROM matches WHERE winner_id = players.id),
+        (SELECT count(*) AS matches FROM matches WHERE winner_id = players.id OR looser_id = players.id)
+    FROM players
+    ORDER BY wins DESC;
+
+/*
+CREATE VIEW pairings AS
+    SELECT id AS id1, name AS name1 FROM standings LIMIT 1 OFFSET 0
+*/
